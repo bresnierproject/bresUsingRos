798c48769f0c3d2406bf1b2e826778d4a5fc8ffd
bresnierproject <32740017+bresnierproject@users.noreply.github.com>
2017-10-14 11:02:31 -0300

    Update especificaciones_del_robot.h

--- other and info/motors/especificaciones_del_robot.h
+++ other and info/motors/especificaciones_del_robot.h
@@ -5,7 +5,7 @@
 #define relacion_transformacion    131.25  // relación de transformación propia del motorreductor c/encoder
 #define diametro_ruedas            0.12   //diámetro en m  original 
 #define ancho_ruedas               0.037   // ancho o espesor de las ruedas en m
-#define distancia_ruedas           0.42   // distancia de separación de las ruedas en m 
+#define distancia_ruedas           0.40   // distancia de separación de las ruedas en m 
 #define MAX_RPM                    80    // velocidad máxima de los motorreductores sin carga al parecer 
 #define pi                         3.1415926  
 #define dos_pi                     6.2831853
